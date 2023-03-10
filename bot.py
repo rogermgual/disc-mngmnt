c18254f224119c28127f3d614b364b9a8eca0e04
Roger Martín Gual <rogermgual>
2023-03-11 00:40:52 +0100

    intents=intents

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 TOKEN = os.environ["WIPE_TOKEN"]
 
 intents = discord.Intents.default()
-client = discord.Client(intents)
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
